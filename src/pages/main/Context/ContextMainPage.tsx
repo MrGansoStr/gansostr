1028a2d9ed83e7e9e5dbcbb2256d636590717050
MrGansoStr <ccounttnro1@gmail.com>
2026-06-29 17:16:16 -0500

    chore(cleanup): remove unused comments

--- src/pages/main/Context/ContextMainPage.tsx
+++ src/pages/main/Context/ContextMainPage.tsx
@@ -1,30 +1,27 @@
 import { createContext, useContext, useState } from "react";
-// import { CoordsFace } from "../../../StyledComponents/ImageCrop";
 
 interface ContextType {
-    modalOpen: boolean;
-    setModalOpen: Function;
-
-    idComponent: string;
-    setIdComponent: Function;
-
+  modalOpen: boolean;
+  setModalOpen: Function;
+  idComponent: string;
+  setIdComponent: Function;
 }
 
 const ContextMainPage = createContext<ContextType | undefined>(undefined);
 
-export const ContextCompMainPage= ({ children }: { children: any }) => {
-    const [modalOpen, setModalOpen] = useState(false);
-    const [idComponent, setIdComponent] = useState("");
+export const ContextCompMainPage = ({ children }: { children: any }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [idComponent, setIdComponent] = useState("");
 
-    return (
-    <ContextMainPage.Provider 
+  return (
+    <ContextMainPage.Provider
       value={{
         modalOpen,
         setModalOpen,
         idComponent,
         setIdComponent
       }}
-      >
+    >
       {children}
     </ContextMainPage.Provider>
   );
