eb4e2b52af473e8253213addd550cd84a9f89501
MrGansoStr <ccounttnro1@gmail.com>
2026-06-29 18:36:39 -0500

    refactor(background): use background from react bits

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -2,6 +2,7 @@ import { ContextCompMainPage } from "./Context/ContextMainPage";
 import { Layout } from "antd";
 import { lazy, useState } from 'react';
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import DotField from "../../components/DotField";
 
 const ModalShow = lazy(() => import('./components/Modal/ModalShow.tsx'));
 const MainTree = lazy(() => import('./components/Tree/MainTree.tsx'));
@@ -36,7 +37,23 @@ const MainPage = () => {
           >
             {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
           </button>
-          <Content>
+          <Content id="main_content">
+            <div id="dotfield_bg">
+              <DotField
+                dotRadius={1.5}
+                dotSpacing={14}
+                bulgeStrength={67}
+                glowRadius={160}
+                sparkle={false}
+                waveAmplitude={0}
+                cursorRadius={500}
+                cursorForce={0.1}
+                bulgeOnly
+                gradientFrom="#A855F7"
+                gradientTo="#B497CF"
+                glowColor="#120F17"
+              />
+            </div>
             <h1 id="main_title">Data Science</h1>
             <MainTree />
           </Content>
